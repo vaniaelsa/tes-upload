af0c8975a4af5fd9780c63db2a2107f38419fcff
vaniaelsa <vaniaelsa99@gmail.com>
2022-10-23 03:08:12 +0700

    practice-1

---
+++ practice-1/App.js
@@ -0,0 +1,14 @@
+import Home from "./component/Home"
+import "./App.css"
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Daftar Pengunjung </h1>
+      <h3> Stasiun Gubeng</h3>
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
